cd0682316fa59e632c295062bcb79a075b9b207d
quabenaboahen <boahendeycarter@gmail.com>
2020-07-20 20:17:17 -0400

    modified serializers.py

--- crowdsourcing/serializers.py
+++ crowdsourcing/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from crowdusers.models import *
 from rest_framework.authtoken.models import Token
+import datetime as dt
+from datetime import datetime
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -24,7 +26,25 @@ class ImageSerializer(serializers.ModelSerializer):
 
 class DeploymentSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False)
+    deployment_image = serializers.SerializerMethodField('dep_image')
+    days_since_deployment = serializers.SerializerMethodField('days_since_dep')
+
+    def dep_image(self, obj):
+        deployment_img_file = DeploymentImages.objects.filter(deployment_number=obj.deployment_number).first()
+        image_serializer = ImageSerializer(instance=deployment_img_file)
+        return image_serializer.data
+
+    def days_since_dep(self, obj):
+        date_format = '%Y-%m-%d'
+        current_date = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), date_format)
+        formatted_deployment_date = datetime.strptime(
+            dt.datetime.strptime(obj.report_date, '%d.%m.%Y').strftime('%Y-%m-%d'), date_format)
+        date_diff = current_date - formatted_deployment_date
+        return date_diff.days
 
     class Meta:
         model = Deployment
-        fields = '__all__'
+        fields = ('report_brief_description', 'report_nature', 'report_full_description', 'reporter_background',
+                  'deployment_location', 'latitude', 'longitude', 'report_date', 'report_time',
+                  'report_time_frame', 'report_response_bodies', 'report_video_link', 'report_views',
+                  'deployment_number', 'report_platform', 'created_date', 'user', 'deployment_image', 'days_since_deployment')
--- crowdsourcing/settings.py
+++ crowdsourcing/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '1yxs04%_ke5a$+(60i_z@2b7hmpjm@3#0-j#%25imiyyyq&w3('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.22.162.119', 'localhost', '127.0.0.1',]
 
 
 # Application definition
